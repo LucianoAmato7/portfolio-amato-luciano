d0861a7b513deff41230877338e8b23dedce739f
LucianoAmato <luciano2697@outlook.com.ar>
2023-03-02 22:05:12 -0300

    Titulo-Principal-Y-TechnologiesBar

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import '../sass/_title.scss'
+
+const Title = () => {
+
+    return(
+        <div className='titleContainer'>
+            <span>Luciano Nicolás Amato</span>
+        </div>
+    )
+}
+
+export default Title
